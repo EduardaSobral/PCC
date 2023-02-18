c1ee36c1bee994053d72db48b10083023d2023c9
yasmimffs <yasmim.ffs00@gmail.com>
2023-02-17 23:30:23 -0300

    Subindo models

--- Codigo/PCC/informativo/models.py
+++ Codigo/PCC/informativo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Informativo (models.Model):
+    nome = models.CharField(max_length=100)
+    informativo = models.TextField()
+
+    def _str_(self):
+        return self.nome
--- Codigo/PCC/reuniao/models.py
+++ Codigo/PCC/reuniao/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Reuniao(models.Model):
+    nome = models.CharField(max_length=100)
+    data = models.CharField(max_length=20)
+    horario = models.CharField(max_length=20)
+    informacoes = models.CharField(max_length=300)
+    cargos: CharField(max_length=70)
+
+    def _str_(self):
+        return self.nome
