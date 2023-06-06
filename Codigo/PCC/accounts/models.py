7cc8b0c9c393033188f4d0fa3bc0625b013c0a02
EduardaSobral <sobraldduda0@gmail.com>
2023-06-06 10:08:24 -0300

    atualizações

--- Codigo/PCC/accounts/models.py
+++ Codigo/PCC/accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Cadastro(User):
+
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=15)
     matricula= models.CharField(max_length=15)
